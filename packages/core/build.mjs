c603cd56cbea64105d6c2da06f79625fb0335ea7
Sandro Schleu <sandro.schleu@no-email>
2024-06-21 15:32:20 +0200

    Improve build

--- packages/core/build.mjs
+++ packages/core/build.mjs
@@ -11,8 +11,25 @@ await esbuild.build({
   banner: {
     js: '// (c) 2024 Sandro Schleu, License MIT',
   },
-  outdir: 'dist',
+  outfile: 'dist/index.js',
   format: 'esm',
+  target: 'esnext',
+  platform: 'neutral',
+});
+
+await esbuild.build({
+  entryPoints: ['index.ts'],
+  bundle: true,
+  minifySyntax: true,
+  minifyWhitespace: true,
+  minifyIdentifiers: false,
+  sourcemap: true,
+  treeShaking: true,
+  banner: {
+    js: '// (c) 2024 Sandro Schleu, License MIT',
+  },
+  outfile: 'dist/index.umd.cjs',
+  format: 'cjs',
   target: 'esnext',
   platform: 'neutral',
 });
